f857e659a8d1719398b9a0db9aa508fde555b5c0
namebighourse <masterskihila@gmail.com>
2024-10-20 17:56:32 +0300

    Feat: creating new solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+    module.exports = function reverse (n) {
+    let RevStr = String(Math.abs(n))
+    const newArr = RevStr.split('').reverse().join('');
+    
+  return(newArr)
+
+
+ 
+};
